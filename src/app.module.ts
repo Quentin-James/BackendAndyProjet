51f9b64acbcf0300b5fc2b1f55f9da5e1543caac
Quentin <jamesquentin46@gmail.com>
2025-10-13 00:14:36 +0200

    commencement user service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 //import { AppController } from './Controller/app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { pgConfig } from './config/DbConfig';
 @Module({
-  imports: [TypeOrmModule.forRoot(pgConfig)],
+  imports: [TypeOrmModule.forRoot(pgConfig), UsersModule],
   //controllers: [AppController],
   providers: [AppService],
 })
